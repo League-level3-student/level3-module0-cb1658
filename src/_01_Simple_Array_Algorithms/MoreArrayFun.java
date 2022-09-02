6592b10febe741513a22cc9b6ca342ca0c333893
league <league@imac-109.attlocal.net>
2022-09-02 16:54:24 -0700

    I did
    Level 3
    Module 0
    01
    00 and 01 and MoreArrayFun

--- src/_01_Simple_Array_Algorithms/MoreArrayFun.java
+++ src/_01_Simple_Array_Algorithms/MoreArrayFun.java
@@ -4,23 +4,59 @@ import java.util.Random;
 
 public class MoreArrayFun {
     //1. Create a main method to test the other methods you write.
-
+	public static void main(String[] args) {
+		String [] array = {"a","b","c","d","e","f","g","h","i"};
+		//prin(array);
+		//System.out.println("");
+		//reverse(array);
+		//System.out.println("");
+		//everyother(array);
+		random(array);
+	}
 
 
     //2. Write a method that takes an array of Strings and prints all the Strings in the array.
-
+	public static void prin(String[] charlie) {
+		for(int a = 0; a < charlie.length; a++) {
+			System.out.println(charlie[a]);
+		}
+	}
 
 
     //3. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in reverse order.
+	public static void reverse(String[] charlie) {
+		for(int a = charlie.length-1; a >= 0; a--) {
+			System.out.println(charlie[a]);
+		}
+	}
 
 
 
     //4. Write a method that takes an array of Strings and prints every other String in the array.
-
+	
+	public static void everyother(String[] charlie) {
+		for(int a = 0; a < charlie.length; a++) {
+			if(a % 2 == 0) {
+				System.out.println(charlie[a]);
+			}
+		}
+	
 
     //5. Write a method that takes an array of Strings and prints all the Strings in the array
     //   in a completely random order. Almost every run of the program should result in a different order.
+		public static void random(String[] charlie) {
+			
+		
+			for(int a = 0; a < charlie.length;) {
+				Random randomnum = new Random();
+				int num = randomnum.nextInt(charlie.length);
+				if(charlie[num] != null) {
+					System.out.println(charlie[num]);
+					charlie[num]= null;
+					a++;
+				}
+			}
 
-
+		}
 }
--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -3,16 +3,23 @@ package _01_Simple_Array_Algorithms;
 public class _00_1D_Array_Methods {
     //1. Complete the method so that it returns the sum of all
     //   of the integers in the array being passed in
-    public static int sumIntArray(int[] values) {
-
-        return 0;
-    }
+	
+	public static int sumIntArray(int[] values) {
+		int sum = 0;
+		for(int i = 0; i < values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
 
     //2. Complete the method so that it returns the average of all
     //   of the integers in the array being passed in
     public static double averageIntArray(int[] values) {
-
-        return 0;
+    	double sum = 0;
+    	for(int a = 0; a < values.length; a++) {
+    		sum += values[a];
+    	}
+        return sum/values.length;
     }
 
 
@@ -20,15 +27,26 @@ public class _00_1D_Array_Methods {
     //   array contains the value specified by the second parameter.
     //   It should otherwise return false.
     public static boolean containsIntValue(int[] array, int value) {
-
-        return false;
+    	boolean contains_questionmark = false;
+    	for(int b = 0; b < array.length; b++) {
+    		if(array[b] == value) {
+    			contains_questionmark = true;
+    		}
+    	}
+        return contains_questionmark;
     }
 
     //4. Complete the method so that it returns the index of the,
     //   first instance that the specified value occurs in the array.
     //   If the array does not contain the specified value, it should return -1.
     public static int getIndex(int[] arr, int value) {
-
-        return 0;
+    	int index = -1;
+    	for(int c = 0; c< arr.length; c++) {
+    		if(arr[c] == value) {
+    			findex = c;
+    			return index;
+    		}
+    	}
+        return index;
     }
 }
